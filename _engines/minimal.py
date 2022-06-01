ab1822d31e05a12b6b6c67bbb57f388c7dc98ec6
soderlundf <62197952+soderlundf@users.noreply.github.com>
2022-06-01 22:30:00 +0200

    Created a minimal SaltStack engine.
    This will be loaded by the salt-master and executed on a regular interval (I've seen every 10 seconds).

---
+++ _engines/minimal.py
@@ -0,0 +1,18 @@
+"""
+Minimal SaltStack engine.
+
+:configuration:
+
+    Example configuration
+
+    .. code-block:: yaml
+
+        engines:
+          - minimal
+
+:depends: nothing
+"""
+
+def start():
+    """Start method is required."""
+    pass
